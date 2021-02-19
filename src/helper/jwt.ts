ce363d2d0fd2b0a16a81ca391f45bf86d93de157
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-02-19 12:25:54 -0300

    feat: adição de helper - jwt

---
+++ src/helper/jwt.ts
@@ -0,0 +1,34 @@
+import { IncomingHttpHeaders } from 'http';
+import { sign, verify } from 'jsonwebtoken';
+import { Payload, VerifyJwtPayload } from '../@types/jwt';
+require('dotenv').config();
+
+const TOKEN_PRIVATE_KEY = process.env.JWT_TOKEN_PRIVATE_KEY; // secret key - .env
+const REFRESH_TOKEN_PRIVATE_KEY = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY; // secret key - .env
+
+const options = { expiresIn: '30 minutes' };
+const refreshOptions = { expiresIn: '30 days' };
+
+export const generateJwt = (payload: Payload) => {
+  return sign(payload, TOKEN_PRIVATE_KEY, options);
+};
+
+export const verifyJwt = (token: string) => {
+  return verify(token, TOKEN_PRIVATE_KEY) as VerifyJwtPayload;
+};
+
+export const generateRefreshJwt = (payload: Payload) => {
+  return sign(payload, REFRESH_TOKEN_PRIVATE_KEY, refreshOptions);
+};
+
+export const verifyRefreshJwt = (token: string) => {
+  return verify(token, REFRESH_TOKEN_PRIVATE_KEY) as VerifyJwtPayload;
+};
+
+export const getTokenFromHeaders = (
+  headers: IncomingHttpHeaders
+): string | null => {
+  const token = headers['authorization'];
+
+  return token ? token.slice(7, token.length) : null;
+};
