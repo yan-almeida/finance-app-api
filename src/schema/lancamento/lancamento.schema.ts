3469b5c90ada2e473164bc6d235b18f6dde652f6
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-02-20 20:13:36 -0300

    feat: adição de esquema de lancamento - salvar

---
+++ src/schema/lancamento/lancamento.schema.ts
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+import { pt } from 'yup-locale-pt';
+
+Yup.setLocale(pt);
+
+export const salvarSchema = Yup.object({
+  categoria: Yup.string(),
+  descricao: Yup.string().min(3).max(500),
+  valor: Yup.number(),
+  nome: Yup.string().min(3).max(300),
+});
