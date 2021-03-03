d57ecea5141cd17e06ed63ee735597ca284a7ef4
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-03-03 20:54:10 -0300

    temp: commit temporário

--- src/util/pagedList.ts
+++ src/util/pagedList.ts
@@ -5,13 +5,8 @@ import { PagedList } from '../@types/PagedList';
 export const pagedList = async <T = any>(
   queryBuilder: Repository<T> | any,
   options: IPaginationOptions
-): Promise<PagedList<T>> => {
+): Promise<T[]> => {
   const paged = await paginate<T>(queryBuilder, options);
 
-  return {
-    items: paged.items,
-    ...paged.meta,
-    hasPreviousPage: paged.meta.currentPage > 1,
-    hasNextPage: paged.meta.currentPage < paged.meta.totalPages,
-  };
+  return paged.items;
 };
